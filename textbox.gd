7561757fa2adcf0267e40dc0f40bd34ccec4a8fc
crystal <cheesedrone@gmail.com>
2026-03-15 16:06:28 -0700

    sign/textboxes, not finished yet tho

---
+++ textbox.gd
@@ -0,0 +1,83 @@
+extends CanvasLayer
+
+const CHAR_READ_RATE = 0.05
+
+@onready var textbox_container = $textboxc
+@onready var start_symbol = $textboxc/MarginContainer/HBoxContainer/start
+@onready var end_symbol = $textboxc/MarginContainer/HBoxContainer/end
+@onready var label = $textboxc/MarginContainer/HBoxContainer/label
+
+enum State {
+	READY,
+	READING,
+	FINISHED
+}
+
+var current_state = State.READY
+var text_queue: Array[String] = []
+var tween: Tween
+
+func _ready():
+	print("Starting state: State.READY")
+	hide_textbox()
+	queue_text("Excuse me wanderer where can I find the bathroom?")
+	queue_text("Why do we not look like the others?")
+	queue_text("Because we are free assets from opengameart!")
+	queue_text("Thanks for watching!")
+
+func _process(delta):
+	match current_state:
+		State.READY:
+			if !text_queue.is_empty():
+				display_text()
+
+		State.READING:
+			if Input.is_action_just_pressed("ui_accept"):
+				label.visible_ratio = 1.0
+				if tween:
+					tween.kill()
+				end_symbol.text = "v"
+				change_state(State.FINISHED)
+
+		State.FINISHED:
+			if Input.is_action_just_pressed("ui_accept"):
+				change_state(State.READY)
+				hide_textbox()
+
+func queue_text(next_text: String):
+	text_queue.push_back(next_text)
+
+func hide_textbox():
+	start_symbol.text = ""
+	end_symbol.text = ""
+	label.text = ""
+	textbox_container.hide()
+
+func show_textbox():
+	start_symbol.text = "*"
+	textbox_container.show()
+
+func display_text():
+	var next_text = text_queue.pop_front()
+	label.text = next_text
+	label.visible_ratio = 0.0
+	
+	change_state(State.READING)
+	show_textbox()
+
+	tween = create_tween()
+	tween.tween_property(label, "visible_ratio", 1.0, next_text.length() * CHAR_READ_RATE)
+
+	await tween.finished
+	end_symbol.text = "v"
+	change_state(State.FINISHED)
+
+func change_state(next_state):
+	current_state = next_state
+	match current_state:
+		State.READY:
+			print("Changing state to: State.READY")
+		State.READING:
+			print("Changing state to: State.READING")
+		State.FINISHED:
+			print("Changing state to: State.FINISHED")
